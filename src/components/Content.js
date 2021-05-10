03c2d6a20a7010bbbc83f199dc83a798256611cc
Samuel <bdzsamuel@gmail.com>
2021-05-10 13:30:58 +0200

    Added initial boilerplate. - Setted Bulma CSS

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="container">
+      <div className="box">
+        <h1 class="title has-text-centered">Making a faboulous portfolio for me!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Content;
